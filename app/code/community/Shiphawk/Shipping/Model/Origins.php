58779e32ad7b5301045af87272cfe958df87ec70
pm <pm@whidegroup.com>
2015-07-09 15:57:04 +0300

    1.1.0 version of plugin

--- app/code/community/Shiphawk/Shipping/Model/Origins.php
+++ app/code/community/Shiphawk/Shipping/Model/Origins.php
@@ -1,8 +1,9 @@
 <?php
-class Shiphawk_Shipping_Model_Resource_Origins extends Mage_Core_Model_Mysql4_Abstract
+class Shiphawk_Shipping_Model_Origins extends Mage_Core_Model_Abstract
 {
+
     protected function _construct()
     {
-        $this->_init('shiphawk_shipping/origins', 'id');
+        $this->_init('shiphawk_shipping/origins');
     }
 }
