70924a2cb1227849b9ffc051a924477fd04031e0
kanishkareddy <kanishkareddy@Kanishkas-MacBook-Pro.local>
2015-10-21 17:59:11 -0700

    Added a global setting to enable or disable this feature

--- app/code/community/Shiphawk/Checkout/Model/Cart.php
+++ app/code/community/Shiphawk/Checkout/Model/Cart.php
@@ -10,33 +10,41 @@ class Shiphawk_Checkout_Model_Cart extends Mage_Checkout_Model_Cart
     public function save($includeRates = false)
     {
         Mage::dispatchEvent('checkout_cart_save_before', array('cart'=>$this));
-        //retrieving previous subtotal form cookie
-        $previousCartSubTotal = Mage::getSingleton('core/session')->getData('previousCartSubTotal', true);
 
-        $this->getQuote()->getBillingAddress();
-        $this->getQuote()->getShippingAddress();
-        $this->getQuote()->collectTotals();
+        $calcRateOnCartChange = Mage::helper('shiphawk_shipping')->getCalcRateOnCartChange();
 
-        if($includeRates) {
-
-            $this->getQuote()->getShippingAddress()->setCollectShippingRates(true);
+        if($calcRateOnCartChange) {
+            parent::save();
         }
-        else if($previousCartSubTotal != $this->getQuote()->getSubtotal()){
+        else {
 
-            $this->getQuote()->getShippingAddress()->removeAllShippingRates();
-            $this->getQuote()->getShippingAddress()->setShippingMethod('')->setShippingDescription('');
+            //retrieving previous subtotal form cookie
+            $previousCartSubTotal = Mage::getSingleton('core/session')->getData('previousCartSubTotal', true);
+
+            $this->getQuote()->getBillingAddress();
+            $this->getQuote()->getShippingAddress();
+            $this->getQuote()->collectTotals();
+
+            if ($includeRates) {
+
+                $this->getQuote()->getShippingAddress()->setCollectShippingRates(true);
+            } else if ($previousCartSubTotal != $this->getQuote()->getSubtotal()) {
+
+                $this->getQuote()->getShippingAddress()->removeAllShippingRates();
+                $this->getQuote()->getShippingAddress()->setShippingMethod('')->setShippingDescription('');
+            }
+
+            $this->getQuote()->collectTotals();
+            $this->getQuote()->save();
+            $this->getCheckoutSession()->setQuoteId($this->getQuote()->getId());
+            /**
+             * Cart save usually called after changes with cart items.
+             */
+
+            Mage::getSingleton('core/session')->setData('previousCartSubTotal', $this->getQuote()->getSubtotal());
+
+            Mage::dispatchEvent('checkout_cart_save_after', array('cart' => $this));
+            return $this;
         }
-
-        $this->getQuote()->collectTotals();
-        $this->getQuote()->save();
-        $this->getCheckoutSession()->setQuoteId($this->getQuote()->getId());
-        /**
-         * Cart save usually called after changes with cart items.
-         */
-
-        Mage::getSingleton('core/session')->setData('previousCartSubTotal',$this->getQuote()->getSubtotal());
-
-        Mage::dispatchEvent('checkout_cart_save_after', array('cart'=>$this));
-        return $this;
     }
 }
